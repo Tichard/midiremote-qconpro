caaada9b205b6033ce69ae59c66747b35153b22f
Tichard <richard.taupiac@gmail.com>
2024-11-08 23:35:50 +0100

    [FIX] Fix calling NPM form bat file. + Add traces.

--- install.bat
+++ install.bat
@@ -1,2 +1,6 @@
-npm run build
-robocopy "Z:\git\midiremote-qconpro\dist" "C:\Users\richa\Documents\Steinberg\Cubase LE AI Elements\MIDI Remote\Driver Scripts\Local" /E
+
+call npm run build
+ECHO:
+ECHO Installing script to Cubase directory...
+robocopy "Z:\git\midiremote-qconpro\dist" "C:\Users\richa\Documents\Steinberg\Cubase LE AI Elements\MIDI Remote\Driver Scripts\Local" /E >nul
+ECHO Done!
